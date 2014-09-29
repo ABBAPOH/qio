41ba0bd536a56c306444612987c205f3fe57d02b
Ivan Komissarov <ABBAPOH@gmail.com>
2014-09-29 22:04:11 +0400

    Revert "Use Runner in dir engine"
    This reverts commit e23d805cf7927bcdf25164526339c0c33ea635b5.

--- src/plugins/file/direngine.cpp
+++ src/plugins/file/direngine.cpp
@@ -1,7 +1,9 @@
 #include "direngine.h"
 
 #include <QIO/FileInfoData>
-#include <QIO/RunnerHelpers>
+#include <QIO/RunExtensions>
+
+#include <QtConcurrent/QtConcurrentRun>
 
 #include <QtCore/QDirIterator>
 #include <QtCore/QFileInfo>
@@ -23,15 +25,8 @@ FileInfo fromQFileInfo(const QFileInfo &info)
     return FileInfo(d);
 }
 
-DirEngine::DirEngine() :
-    runner(new Runner)
+DirEngine::DirEngine()
 {
-}
-
-DirEngine::~DirEngine()
-{
-    runner->waitForDone();
-    delete runner;
 }
 
 QFuture<QString> DirEngine::list()
@@ -45,7 +40,7 @@ QFuture<QString> DirEngine::list()
         }
         return;
     };
-    return RunnerHelpers::run(runner, f, url().toLocalFile());
+    return QtConcurrent::run(f, url().toLocalFile());
 }
 
 QFuture<FileInfo> DirEngine::entryList()
@@ -59,7 +54,7 @@ QFuture<FileInfo> DirEngine::entryList()
         }
         return;
     };
-    return RunnerHelpers::run(runner, f, url().toLocalFile());
+    return QtConcurrent::run(f, url().toLocalFile());
 }
 
 QFuture<bool> DirEngine::mkdir(const QString &dirName)
@@ -69,7 +64,7 @@ QFuture<bool> DirEngine::mkdir(const QString &dirName)
         future.reportResult(QDir(path).mkdir(dirName));
     };
 
-    return RunnerHelpers::run(runner, f, url().toLocalFile(), dirName);
+    return QtConcurrent::run(f, url().toLocalFile(), dirName);
 }
 
 QFuture<bool> DirEngine::remove(const QString &fileName)
--- src/plugins/file/direngine.h
+++ src/plugins/file/direngine.h
@@ -3,13 +3,10 @@
 
 #include <QIO/AbstractDirEngine>
 
-class Runner;
-
 class DirEngine : public AbstractDirEngine
 {
 public:
     DirEngine();
-    ~DirEngine();
 
     QFuture<QString> list() Q_DECL_OVERRIDE;
     QFuture<FileInfo> entryList() Q_DECL_OVERRIDE;
@@ -17,9 +14,6 @@ public:
     // TODO: separate rmdir?
     QFuture<bool> remove(const QString &fileName) Q_DECL_OVERRIDE;
     QFuture<FileInfo> stat(const QString &fileName) Q_DECL_OVERRIDE;
-
-private:
-    Runner *runner;
 };
 
 #endif // FILEHANDLER_H
