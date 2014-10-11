6902c375eb89df1f597695d8efd9613e5b5f1137
Ivan Komissarov <ABBAPOH@gmail.com>
2014-10-12 03:52:01 +0400

    Add RunHelpers::run that takes 3 arguments

--- src/lib/runnerhelpers.h
+++ src/lib/runnerhelpers.h
@@ -97,6 +97,34 @@ private:
     Arg1 arg1; Arg2 arg2;
 };
 
+template <typename T,  typename FunctionPointer, typename Arg1, typename Arg2, typename Arg3>
+class StoredInterfaceFunctionCall3 : public QRunnable
+{
+public:
+    StoredInterfaceFunctionCall3(Runner *runner, void (fn)(QFutureInterface<T> &, Arg1, Arg2, Arg3), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3) :
+        runner(runner), fn(fn), arg1(arg1), arg2(arg2), arg3(arg3) { }
+
+    QFuture<T> start()
+    {
+        futureInterface.reportStarted();
+        QFuture<T> future = futureInterface.future();
+        runner->start(this);
+        return future;
+    }
+
+    void run()
+    {
+        fn(futureInterface, arg1, arg2, arg3);
+        futureInterface.reportFinished();
+    }
+
+private:
+    Runner *runner;
+    QFutureInterface<T> futureInterface;
+    FunctionPointer fn;
+    Arg1 arg1; Arg2 arg2; Arg3 arg3;
+};
+
 template <typename T>
 QFuture<T> run(Runner *runner, void (*functionPointer)(QFutureInterface<T> &))
 {
@@ -113,6 +141,12 @@ template <typename T, typename Arg1, typename Arg2>
 QFuture<T> run(Runner *runner, void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2), const Arg1 &arg1, const Arg2 &arg2)
 {
     return (new StoredInterfaceFunctionCall2<T, void (*)(QFutureInterface<T> &, Arg1, Arg2), Arg1, Arg2>(runner, functionPointer, arg1, arg2))->start();
+}
+
+template <typename T, typename Arg1, typename Arg2, typename Arg3>
+QFuture<T> run(Runner *runner, void (*functionPointer)(QFutureInterface<T> &, Arg1, Arg2, Arg3), const Arg1 &arg1, const Arg2 &arg2, const Arg3 &arg3)
+{
+    return (new StoredInterfaceFunctionCall3<T, void (*)(QFutureInterface<T> &, Arg1, Arg2, Arg3), Arg1, Arg2, Arg3>(runner, functionPointer, arg1, arg2, arg3))->start();
 }
 
 } // namespace RunnerHelpers
