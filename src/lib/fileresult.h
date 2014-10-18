8a1f4ce7fa41601c8609da3ea4d2b71c422fa487
Ivan Komissarov <ABBAPOH@gmail.com>
2014-10-18 10:57:19 +0400

    Make FileResult inline

--- src/lib/fileresult.h
+++ src/lib/fileresult.h
@@ -17,20 +17,20 @@ public:
         NotImplemented
     };
 
-    FileResult();
-    FileResult(Error error);
+    inline Q_DECL_CONSTEXPR FileResult() : m_error(Error::None) {}
+    inline Q_DECL_CONSTEXPR FileResult(Error error) : m_error(error) {}
 
-    bool hasError() const;
-    Error error() const;
-    QString errorString() const;
+    inline Q_DECL_CONSTEXPR bool hasError() const { return m_error != Error::None; }
+    inline Error Q_DECL_CONSTEXPR error() const { return m_error; }
+    inline QString errorString() const { return errorToString(m_error); }
 
-    void swap(FileResult &other);
+    inline void swap(FileResult &other) { qSwap(m_error, other.m_error); }
 
-    inline operator RestrictedBool() const
+    inline Q_DECL_CONSTEXPR operator RestrictedBool() const
     {
         return hasError() ? Q_NULLPTR : &FileResult::hasError;
     }
-    inline bool operator !() const { return hasError(); }
+    inline bool Q_DECL_CONSTEXPR operator !() const { return hasError(); }
 
 private:
     static QString errorToString(Error error);
