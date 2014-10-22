5d3e1812f0313ee93535f57e14b72d6402122fb3
Ivan Komissarov <ABBAPOH@gmail.com>
2014-10-22 09:59:16 +0400

    Add infoList test for archive

--- tests/auto/archive/tst_archive.cpp
+++ tests/auto/archive/tst_archive.cpp
@@ -33,6 +33,8 @@ public:
 private slots:
     void list_data();
     void list();
+    void infoList_data();
+    void infoList();
 
 private:
 };
@@ -64,6 +66,65 @@ void tst_Archive::list()
     auto future = entry.list(QDir::NoDotAndDotDot | QDir::AllEntries, QDir::Name);
     future.waitForFinished();
     QCOMPARE(future.result(), list);
+}
+
+struct Info
+{
+    Info() {}
+    Info(bool isDir, const QString &name) : isDir(isDir), name(name) {}
+
+    bool isDir;
+    QString name;
+};
+
+typedef QVector<Info> InfoList;
+
+Q_DECLARE_METATYPE(Info)
+
+void tst_Archive::infoList_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<InfoList>("list");
+
+    QTest::newRow("empty") << QString("empty.zip") << (InfoList());
+    QTest::newRow("plain") << QString("plain.zip") << (InfoList()
+                                                       << Info(false, "freedesktop.org.xml")
+                                                       << Info(false, "medium.txt")
+                                                       << Info(false, "small.txt"));
+}
+
+static bool compareInfos(const FileInfo &fileInfo, const Info &info)
+{
+    return fileInfo.isDir() == info.isDir
+            && fileInfo.fileName() == info.name;
+}
+
+static bool compareInfos(const FileInfoList &fileInfos, const InfoList &infos)
+{
+    if (infos.count() != fileInfos.count())
+        return false;
+
+    for (int i = 0; i < infos.count(); ++i) {
+        if (!compareInfos(fileInfos.at(i), infos.at(i)))
+            return false;
+    }
+    return true;
+}
+
+void tst_Archive::infoList()
+{
+    QTemporaryDir tempDir;
+
+    QFETCH(QString, fileName);
+    QFETCH(InfoList, list);
+    const QString filePath = tempDir.path() + "/" + fileName;
+
+    QVERIFY(::copyFile(":/" + fileName, filePath));
+
+    FileEntry entry(QString("zip://%1").arg(filePath));
+    auto future = entry.infoList(QDir::NoDotAndDotDot | QDir::AllEntries, QDir::Name);
+    future.waitForFinished();
+    QVERIFY(compareInfos(future.result(), list));
 }
 
 QTEST_MAIN(tst_Archive)
